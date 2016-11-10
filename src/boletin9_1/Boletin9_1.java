9094988d87f9f8edcb64f3b2886f39e526cd0b5b
agarridogarcia <agarridogarcia@atanasoff30.danielcastelao.org>
2016-11-10 12:05:06 +0100

    a

---
+++ src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,16 @@
+
+package boletin9_1;
+
+import javax.swing.JOptionPane;
+
+public class Boletin9_1 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        Numeros num1=new Numeros();
+                num1.numeros();
+    }
+    
+}
---
+++ src/boletin9_1/Numeros.java
@@ -0,0 +1,38 @@
+
+package boletin9_1;
+
+import javax.swing.JOptionPane;
+
+
+public class Numeros {
+    
+    
+    
+    //cont
+    public Numeros(){
+    }
+   //metd.
+    public void numeros(){
+        int num,cp=0, cn=0, cc=0;
+        for (int i=1; i<10; i++){
+            num=Integer.parseInt(JOptionPane.showInputDialog("Introduce numero : "));
+            
+        
+        if(num>0) 
+            cp++;
+                
+        else if (num<0)
+            cn++;
+        else
+            cc++;
+        }  
+   
+       System.out.println("Positivos =  " + cp + " Negativos =  " + cn + " ceros = " + cc );
+        
+   }
+        
+                
+                
+  }
+    
+
